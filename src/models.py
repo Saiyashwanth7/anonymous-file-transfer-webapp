2fdef350deba4d45f66856619f0442bfcdf184ba
Shuchasmitha <saiyashwanthdasari@gmail.com>
2025-07-31 09:41:55 +0530

    Token, Created and Expire time included

--- src/models.py
+++ src/models.py
@@ -1,9 +1,13 @@
-from sqlalchemy import Column,Integer,String
+from sqlalchemy import Column,Integer,String,DATETIME
 from database import Base
+from datetime import timezone,datetime,timedelta
+import secrets
 
 class Share(Base):
     __tablename__="filestorage"
     id=Column(Integer,primary_key=True,index=True)
     file_name=Column(String,unique=True)
     file_path=Column(String)
-    
+    token=Column(String,unique=True,index=True,default=lambda: secrets.token_urlsafe(32))
+    created=Column(DATETIME,default=lambda: datetime.now(timezone.utc))
+    expires=Column(DATETIME,default=lambda: datetime.now(timezone.utc) + timedelta(minutes=1))
