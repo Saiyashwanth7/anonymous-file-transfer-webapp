11cbdcc815276054770cddbe1480629783c0fbea
Shuchasmitha <saiyashwanthdasari@gmail.com>
2025-08-16 22:15:56 +0530

    Yet to add cleanup functionalities, for group mailing

--- src/models.py
+++ src/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column,Integer,String,DATETIME
+from sqlalchemy import Column,Integer,String,DATETIME,ForeignKey
 from database import Base
 from datetime import timezone,datetime,timedelta
 import secrets
@@ -6,8 +6,20 @@ import secrets
 class Share(Base):
     __tablename__="filestorage"
     id=Column(Integer,primary_key=True,index=True)
-    file_name=Column(String,unique=True)
+    file_name=Column(String)
     file_path=Column(String)
+    token=Column(String,unique=True,index=True,default=lambda: secrets.token_urlsafe(32))
+    created=Column(DATETIME,default=lambda: datetime.now(timezone.utc))
+    expires=Column(DATETIME,default=lambda: datetime.now(timezone.utc) + timedelta(hours=24))
+    file_type=Column(String)
+
+class GroupShare(Base):
+    __tablename__="grouptable"
+    id=Column(Integer,primary_key=True,index=True)
+    #share_id=Column(Integer,ForeignKey("filestorage.id"))
+    file_name=Column(String)
+    file_path=Column(String)
+    receiver_email=Column(String)
     token=Column(String,unique=True,index=True,default=lambda: secrets.token_urlsafe(32))
     created=Column(DATETIME,default=lambda: datetime.now(timezone.utc))
     expires=Column(DATETIME,default=lambda: datetime.now(timezone.utc) + timedelta(minutes=1))
